0099af3da1ca6236d3f6ca4832fb39e01ba70ae5
Luke Terry <luke.terry0682@gmail.com>
2021-12-03 20:52:27 -0600

    Rewrite Circle Grid Class

--- collection/sketch1/sketch.js
+++ collection/sketch1/sketch.js
@@ -1,47 +1,56 @@
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	background("#d9d9d9")
-	translate(-width/2, -height/2)
-	cg = circleGrid(10, 25,7,4,[3,4,5,7,14, 30])
+	background("grey")
 	img = loadImage("pics/che.png")
+	img.loadPixels();
+	cg = new gridOfCircles(0,0,10,10,50,50);
+	
+
 }
 
 function draw() {
-
+	//image(img, 0,0)
+	//circle(0,0,50)
+	circle(windowWidth,windowHeight, 50)
 	cg.draw();
 }
 
-//create object constructor for circle grid
-//intakes width, height, largeRadius, smallRadius, and a list of circles to draw small
-//small circles list is simply sequential starting at 0,0, going to 0, n, then to 1, 0 and so on
-function circleGrid(w, h, largeRadius, smallRadius, smallCircleList) {
-	//grid of circles
-	this.width = w;
-	this.height = h;
-	this.Br = largeRadius;
-	this.Sr = smallRadius;
-	this.smallCircles = smallCircleList
-	this.spacing = (windowWidth/2 - this.Br) / w
 
-	this.draw = function(){
-		//aesthetic settings
-		fill("#291007")
-		stroke("#291007")
+class gridOfCircles{
+	//creates a class for a gridOfCircles object to draw a grid of circles, where each circle's size can be changed through a 2d array
+	constructor(cornerx,cornery,width,length,standardDiameter, spacing){
+		this.cx = cornerx;
+		this.cy = cornery;
+		this.width = width;
+		this.length = length;
+		this.d = standardDiameter;
+		this.spc = spacing;
+		//construct a 2dArray to store the diameters of the circles
+		this.grid =  Array.from(Array(length), () => {
+			return new Array(width).fill(this.d)
+		})
+	}
 
+	changeDiameters(circlesToChange, newDiameter){
+		//changes the radii of certain circles in the grid, where the 2d array == true
+		for (let i = 0; i < circlesToChange.length; i++){
+			let x = circlesToChange[i][0];
+			let y = circlesToChange[i][1];
 
-		//draw the grid of circles
-		for (j = 0; j < this.height; j++){
-			for (i = 0; i < this.width; i++){
+			this.grid[x][y] = newDiameter;
+			console.log(this.grid)
+		}
+	}
 
-				if (this.smallCircles.indexOf((j*this.width + i)) == -1){
-					//if the circle is meant to be big
-					circle(i*this.spacing + Br, j*this.spacing + Br, this.Br);
-				} else{
-					//if the circle is meant to be small
-					circle(i*this.spacing + Br, j*this.spacing + Br, this.Sr);
-				}
-
+	draw(){
+		//draws the grid of circles
+		
+		for (let i = 0; i < 10; i++){
+			for (let j = 0; j < 10; j++){
+				circle(this.cx + this.spc*i, this.cy + this.spc*j, this.grid[i][j])
 			}
 		}
 	}
+
 }
+
